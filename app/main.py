5358d8f992b29ebd70d13cc5db2a7d0c252dcfe3
мd ѕαℓαнuddιn <salahuddin3652@gmail.com>
2024-08-18 19:17:35 +0600

    exposing headers

--- app/main.py
+++ app/main.py
@@ -60,7 +60,8 @@ app.add_middleware(
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=['*'],
-    allow_headers=['*']
+    allow_headers=['*'],
+    expose_headers=['X-Total-Count', 'X-Total-Pages', 'X-Current-Page', 'X-Per-Page']
 )
 
 app.include_router(api_router)
