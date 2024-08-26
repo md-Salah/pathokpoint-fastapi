b239f42edd50e2e27ac1238033fc1348abd837f5
мd ѕαℓαнuddιn <salahuddin3652@gmail.com>
2024-08-27 01:44:27 +0600

    statusIn fixed

--- app/pydantic_schema/order_status.py
+++ app/pydantic_schema/order_status.py
@@ -1,4 +1,3 @@
-from pydantic import UUID4
 from typing import ClassVar
 
 from app.pydantic_schema.base import BaseModel
@@ -9,18 +8,14 @@ from app.constant.orderstatus import Status
 class StatusBase(BaseModel):
     status: Status
     note: str | None = None
-    _admin_note: str | None = None
     
     _example: ClassVar = {
         'status': Status.order_confirmed,
         'note': 'This is a note',
-        '_admin_note': 'This is an admin note',
     }
 
 class StatusIn(StatusBase):
-    admin_id: UUID4 | None = None
     _example: ClassVar = {
-        'admin_id': 'valid-uuid4',
         **StatusBase._example
     }
     
