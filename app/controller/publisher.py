b206362a975be3929805265139a890b841cc0d00
мd ѕαℓαнuddιn <salahuddin3652@gmail.com>
2024-09-18 01:36:51 +0600

    publisher missing greenlet fix

--- app/controller/publisher.py
+++ app/controller/publisher.py
@@ -1,5 +1,6 @@
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import select, func, or_, selectinload, joinedload
+from sqlalchemy import select, func, or_
+from sqlalchemy.orm import selectinload, joinedload
 from typing import Sequence
 from uuid import UUID
 import logging
