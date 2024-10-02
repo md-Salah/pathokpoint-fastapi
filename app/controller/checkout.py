870bf9286f6cb31adcbe4948b9fcfb45d282f5a9
мd ѕαℓαнuddιn <salahuddin3652@gmail.com>
2024-10-02 15:01:46 +0600

    free shipping bug fix

--- app/controller/checkout.py
+++ app/controller/checkout.py
@@ -9,10 +9,12 @@ logger = logging.getLogger(__name__)
 
 async def checkout_summary(payload: dict[str, Any], db: AsyncSession):
     order = await order_service.create_order(payload, db, commit=False)
-    return {
+    data = {
         'sub_total': order.old_book_total + order.new_book_total,
         'shipping_charge': order.shipping_charge,
         'weight_charge': order.weight_charge,
         'discount': order.discount,
         'coupon_code': payload.get('coupon_code')
     }
+    logger.debug(f'Checkout summary: {data}')
+    return data
